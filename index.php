b431de8a22c3eb9c2a8e5dc7b1ae504e067eb047
Subhan Mohammed <108663284+Subhan70@users.noreply.github.com>
2024-05-18 13:09:34 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- index.php -->
+<html>
+<head><title>PHP Form</title></head>
+<body>
+    <h2>Enter Data</h2>
+    <form action="submit.php" method="post">
+        Name: <input type="text" name="name"><br>
+        Email: <input type="email" name="email"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
