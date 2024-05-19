022c92e8aa5f2f5d8c6e61600936a986a292bf58
Subhan Mohammed <108663284+Subhan70@users.noreply.github.com>
2024-05-19 12:40:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head><title>PHP Form</title></head>
 <body>
     <h2>Enter Data</h2>
-    <form action="submit.php" method="post">
+    <form action="samplePage.php" method="post">
         Name: <input type="text" name="name"><br>
         Email: <input type="email" name="email"><br>
         <input type="submit">
